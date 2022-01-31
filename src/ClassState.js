19636524fc187d90e6ecd6b0c791c8d86a259bb2
Leinerke Suarez <leinerke2000@gmail.com>
2022-01-31 11:07:50 -0500

    Estados simples: React.Component vs. useState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: true,
+    };
+  }
+
   render() {
+    const { name } = this.props;
     return (
       <div>
-        <h2>Eliminar ClassState</h2>
+        <h2>Eliminar {name}</h2>
         <p>Por favor escribe el codigo de seguridad</p>
+        {!!this.state.error && (
+          <p>Error: El codigo es incorrecto</p>
+        )}
         <input
           placeholder="Codigo de seguridad"
         />
-        <button>Comprobar</button>
+        <button
+          onClick={() => this.setState(prevState => ({ error: !prevState.error }))}
+        >
+          Comprobar
+        </button>
       </div>
     );
   }
